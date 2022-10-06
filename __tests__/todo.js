cd26530733806e250e4ad675a4c7a44f2315fe12
Melson Zacharias <jmelson7@gmail.com>
2022-10-06 17:19:16 +0530

    Add L9

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,14 +1,20 @@
+var cheerio = require("cheerio");
 const request = require("supertest");
 
 const db = require("../models/index");
 const app = require("../app");
 
 let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
 describe("List the todo items", function () {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     server = app.listen(3000, () => {});
-    agent = request.agent(server);
   });
 
   afterAll(async () => {
@@ -16,34 +22,56 @@ describe("List the todo items", function () {
     server.close();
   });
 
+  test("returns data in specified format", async () => {
+    agent = request.agent(server);
+    const res = await agent.get("/").set("Accept", "application/json");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+
+    const parsedResponse = JSON.parse(res.text);
+    expect(parsedResponse.overdue).toBeDefined();
+    expect(parsedResponse.dueLater).toBeDefined();
+    expect(parsedResponse.dueToday).toBeDefined();
+  });
+
   test("create a new todo", async () => {
+    agent = request.agent(server);
+    const res = await agent.get("/");
+    const csrfToken = extractCsrfToken(res);
     const response = await agent.post("/todos").send({
+      _csrf: csrfToken,
       title: "Buy milk",
       dueDate: new Date().toISOString(),
       completed: false,
     });
-    expect(response.statusCode).toBe(200);
-    expect(response.header["content-type"]).toBe(
-      "application/json; charset=utf-8"
-    );
-    const parsedResponse = JSON.parse(response.text);
-    expect(parsedResponse.id).toBeDefined();
+    expect(response.statusCode).toBe(302);
   });
 
   test("Mark a todo as complete", async () => {
-    const response = await agent.post("/todos").send({
+    agent = request.agent(server);
+    const res = await agent.get("/");
+    const csrfToken = extractCsrfToken(res);
+    await agent.post("/todos").send({
+      _csrf: csrfToken,
       title: "Buy milk",
       dueDate: new Date().toISOString(),
       completed: false,
     });
-    const parsedResponse = JSON.parse(response.text);
-    const todoID = parsedResponse.id;
 
-    expect(parsedResponse.completed).toBe(false);
+    const groupedTodosResponse = await agent
+      .get("/")
+      .set("Accept", "application/json");
+    const parsedGroupedResponse = JSON.parse(groupedTodosResponse.text);
+
+    expect(parsedGroupedResponse.dueToday).toBeDefined();
+
+    const dueTodayCount = parsedGroupedResponse.dueToday.length;
+    const latestTodo = parsedGroupedResponse.dueToday[dueTodayCount - 1];
 
     const markCompleteResponse = await agent
-      .put(`/todos/${todoID}/markASCompleted`)
-      .send();
+      .put(`/todos/${latestTodo.id}`)
+      .send({
+        _csrf: csrfToken,
+      });
     const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
     expect(parsedUpdateResponse.completed).toBe(true);
   });
--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -14,6 +14,54 @@ module.exports = (sequelize, DataTypes) => {
 
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    static async overdue() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
+          completed: false,
+        },
+      });
+    }
+
+    static async dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+          completed: false,
+        },
+      });
+    }
+
+    static async dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
+          completed: false,
+        },
+      });
+    }
+
+    static async completed() {
+      return this.findAll({
+        where: {
+          completed: true,
+        },
+      });
+    }
+    static async remove(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
     }
 
     markAsCompleted() {
